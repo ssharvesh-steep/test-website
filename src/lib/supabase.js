200cc5a5301992158d534bbabc3590a4744fa356
sharvesh <sharvesh@172-1-10-24.lightspeed.lbcktx.sbcglobal.net>
2026-01-27 09:26:53 +0530

    feat: use environment variables for supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gkrqvlbsshhzavclphve.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrcnF2bGJzc2hoemF2Y2xwaHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5NDE3NjcsImV4cCI6MjA4MzUxNzc2N30.zNvnUm0Rrrm8xRmbIT-TIQuJZrSem3CPoVkOe-sieTQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
